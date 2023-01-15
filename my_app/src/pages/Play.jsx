9ca30160edea7a1e5648bfc18edaf6005b9c4d11
SebastianMarquardt <Sebastian.Marquardt99@web.de>
2023-01-15 23:26:03 +0100

    Play functionality added

--- my_app/src/pages/Play.jsx
+++ my_app/src/pages/Play.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import './Play.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Axios from 'axios';
 
 function Play() {
 
+    if (localStorage.getItem('highscore') == null) {
+        localStorage.setItem('highscore', JSON.stringify("0"));
+    }
+    const highscore = localStorage.getItem('highscore')
+
+    const [currentScore, setCurrentScore] = useState(0)
     const [newWord, setNewWord] = useState("")
-    const [errMsg, setErrMsg] = useState("");
+    const [errMsg, setErrMsg] = useState("")
+    const [words, setWords] = useState([])
+
+    const fetchData = () => {
+        fetch("http://localhost:8081/words/getAll")
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error("NETWORK RESPONSE ERROR");
+                }
+            })
+            .then(data => {
+                setWords(data)
+            })
+            .catch((error) => console.error("FETCH ERROR:", error));
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     const addWord = async () => {
         if (newWord.length > 2) {
@@ -16,7 +42,7 @@ function Play() {
                         word: newWord,
                     }
                 })
-                window.location.reload(false);
+                setErrMsg("New word set")
             }
             catch (err) {
                 if (!err?.response) {
@@ -30,15 +56,43 @@ function Play() {
         }
     }
 
+    function randomInt(max) {
+        return Math.floor(Math.random() * (max + 1))
+    }
+
     return (
         <div id='introDiv'>
             <ul>
-                <li><h1>
-                    Try to differentiate between new words, and words you've already seen
-                </h1>
-                    <button>
-                        Start
-                    </button></li>
+                <li>
+                    <h1>
+                        Try to differentiate between new words, and words you've already seen
+                    </h1>
+                </li>
+                <li>
+                    <h2>
+                        Highscore: {highscore}
+                    </h2>
+                </li>
+                <li>
+                    <h2>
+                        current Score: {currentScore}
+                    </h2>
+                </li>
+            </ul>
+            <ul>
+                {words.length > 0 && (
+                    <li>
+                        <h1>{
+                            words[randomInt(words.length - 1)].word
+                        }</h1>
+                    </li>
+                )}
+                <li>
+                    <button>already seen</button>
+                </li>
+                <li>
+                    <button>new in this session</button>
+                </li>
             </ul>
             <ul id='addWord'>
                 <p>{errMsg}</p>
